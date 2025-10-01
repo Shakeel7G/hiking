7d5ba0c9f8926af09432b6dac4ab8635907a6f6c
Shakeel Gafieldien <shakeel.gafieldien@gmail.com>
2025-10-01 09:47:56 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,16 @@
+// db.js
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 const db = mysql.createPool({
-  host: 'sqlXXX.epizy.com',              // your InfinityFree MySQL host
-  user: 'if0_40059353',                  // your database username
-  password: 'shakee2003',              // your database password
-  database: 'if0_40059353_into_the_land', // your actual database name
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
 });
-
 
 export default db;
