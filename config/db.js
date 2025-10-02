4b0d6e1048a338e38462c17d77c61be6e0b1ac7f
Shakeel Gafieldien <shakeel.gafieldien@gmail.com>
2025-10-02 10:18:50 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const db = new Pool({
-  host: process.env.DB_HOST,                  // db.izgzacmosardfrftumdu.supabase.co
+  host: process.env.DB_HOST,                  // e.g., db.izgzacmosardfrftumdu.supabase.co
   user: process.env.DB_USER,                  // Supabase DB username
-  password: process.env.DB_PASS,              // Supabase password
+  password: process.env.DB_PASS,              // Supabase DB password
   database: process.env.DB_NAME,              // Supabase database name
   port: process.env.DB_PORT || 5432,          // Postgres default port
   ssl: { rejectUnauthorized: false },         // required for Render + Supabase
